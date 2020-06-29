cc562048f0864bd40e65643c8fac7ccdc83160ea
Yegor Trussov <egortr2004@mail.ru>
2020-06-29 14:09:00 +0600

    audio play/stop functionality added

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import './App.css'
 export default class App extends Component {
 
     state = {
-        tabs: []
+        tabs: [],
+        isPlaying: false
     }
 
     componentWillMount() {
         let tabs = [], row = [];
-        for (let i = 0; i < 5; i++) 
+        for (let i = 0; i < 8; i++) 
             row.push(false);
         for (let i = 0; i < 3; i++) 
             tabs.push(row);
@@ -34,8 +35,43 @@ export default class App extends Component {
         })
     }    
 
-    render() {
+    sound() {
+        // console.log(this)
         const { tabs } = this.state;
+
+        for (let i = 0; i < tabs[0].length; i++) {
+            setTimeout(() => {
+                for (let j = 0; j < 3; j++) {
+                    let row = tabs[j].slice();
+                    if (row[i]) {
+                        let audio = document.querySelector(`audio.sound-${ j }`);
+                        audio.play()
+                    }
+                }
+            }, 500 * i)
+        }
+    }
+
+    onPlay() {
+        let { isPlaying, tabs } = this.state;
+
+
+        isPlaying = !isPlaying;
+
+        if (isPlaying) {
+            this.sound();
+            this.playInt = setInterval(this.sound.bind(this), tabs[0].length * 500)
+        } else {
+            clearInterval(this.playInt);
+        }
+
+        this.setState({
+            isPlaying
+        })
+    }
+
+    render() {
+        const { tabs, isPlaying } = this.state;
 
         let sounds = ['kick.mp3', 'snare.mp3', 'snare2.mp3'];
         let colors = ['#33ffff', '#33ff66', '#ff3366'];
@@ -48,6 +84,15 @@ export default class App extends Component {
                             <TabRow data={ row } type={ inx } sound={ sounds[inx] } onSelect={ (row, inx) => this.onSelect(row, inx) } color={ colors[inx] } />
                         )
                     })
+                }
+                <button onClick={ () => this.onPlay() } className="play">{ isPlaying ? 'Stop' : 'play' }</button>
+
+                {
+                    sounds.map((sound, inx) => (
+                        <audio className={ `sound-${ inx }` }>
+                            <source src={ `/sounds/${ sounds[inx] }` } />
+                        </audio>
+                    ))
                 }
             </div> 
         )
